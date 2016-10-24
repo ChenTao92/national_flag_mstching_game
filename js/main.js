30f3d980bec0658bada4844e68a333e03a64b458
taochen <taochen@TC-MacBookpro.local>
2016-10-25 04:57:53 +0800

    添加游戏模式切换和难度选择功能

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 * @Author: taochen
 * @Date:   2016-10-17 17:37:39
 * @Last Modified by:   taochen
-* @Last Modified time: 2016-10-24 18:02:20
+* @Last Modified time: 2016-10-25 04:54:52
 */
 
 var row = 6
@@ -47,59 +47,99 @@ function initImage() {
     var $backImage = $('<img/>').attr('src', backImageSrc).addClass('backImage')
     $('.grid').eq(index).append($flagImage, $backImage)
   })
-
-  // add listener,在父元素上做事件代理，避免给每个子元素都加事件处理器
-  $gameArea.on("click", function(evt) {
-    if (noClick) return
-    var $target = $(evt.target)
-    if ($target.hasClass('backImage')) {
-      $target.hide()
-      var $frontImage = $target.siblings()
-      if($click1){
-        noClick = true
-        if($frontImage.attr('src') === $click1.attr('src')){
-          setTimeout(function(){
-            $frontImage.remove()
-            $click1.remove()
-            $click1 = null
-            noClick = false
-          }, duration)
-        }else{
-          setTimeout(function(){
-            $target.show()
-            $click1.siblings().show()
-            $click1 = null
-            noClick = false
-          }, duration)
-        }
-      }else{
-        $click1 = $frontImage
-      }
-    }
-
-
-  })
 }
 
+// add listener,在父元素上做事件代理，避免给每个子元素都加事件处理器
+$gameArea.on("click", function(evt) {
+  if (noClick) return
+  var $target = $(evt.target)
+  if ($target.hasClass('backImage')) {
+    $target.hide()
+    var $frontImage = $target.siblings()
+    if($click1){
+      noClick = true
+      if($frontImage.attr('src') === $click1.attr('src')){
+        setTimeout(function(){
+          $frontImage.remove()
+          $click1.remove()
+          $click1 = null
+          noClick = false
+        }, duration)
+      }else{
+        setTimeout(function(){
+          $target.show()
+          $click1.siblings().show()
+          $click1 = null
+          noClick = false
+        }, duration)
+      }
+    }else{
+      $click1 = $frontImage
+    }
+  }
+})
+
+$('#option1').on('click',function(){
+  $('#option1').parent().addClass('active')
+  $('#option2').parent().removeClass('active')
+  $('.backImage').css('opacity','1.0')
+})
+
+$('#option2').on('click',function(){
+  $('#option1').parent().removeClass('active')
+  $('#option2').parent().addClass('active')
+  $('.backImage').css('opacity','0.7')
+})
+
+$('#option3').on('click',function(){
+  $('#option3').parent().addClass('active')
+  $('#option4').parent().removeClass('active')
+  $('#option5').parent().removeClass('active')
+  row = 4
+  col = 4
+  $click1 = null
+  noClick = false
+  $('#gameArea > div').remove()
+  createGrid()
+  initImage()
+  if($('#option2').parent().hasClass('active')){
+    $('.backImage').css('opacity','0.7')
+  }
+})
+
+$('#option4').on('click',function(){
+  $('#option3').parent().removeClass('active')
+  $('#option4').parent().addClass('active')
+  $('#option5').parent().removeClass('active')
+  row = 6
+  col = 6
+  $click1 = null
+  noClick = false
+  $('#gameArea > div').remove()
+  createGrid()
+  initImage()
+  if($('#option2').parent().hasClass('active')){
+    $('.backImage').css('opacity','0.7')
+  }
+})
+
+$('#option5').on('click',function(){
+  $('#option3').parent().removeClass('active')
+  $('#option4').parent().removeClass('active')
+  $('#option5').parent().addClass('active')
+  row = 8
+  col = 8
+  $click1 = null
+  noClick = false
+  $('#gameArea > div').remove()
+  createGrid()
+  initImage()
+  if($('#option2').parent().hasClass('active')){
+    $('.backImage').css('opacity','0.7')
+  }
+})
 
 createGrid()
 initImage()
 
-// NProgress.start();
-// NProgress.done();
-// function startTimer(){
-//   $('<span></span>').appendTo('.progress-bar');
-//   $('.progress-bar span').width(0);
-//   $('.progress-bar span').width('100%');
-//   setTimeout(function(){ 
-//     $('.progress-bar span').remove();
-//     $(".gameFailure").css('display','block');
-//   },20000);
-// }
-
-
-// function clearTimer(){
-//   clearTimeout(timeOutId);
-//   $('.progress-bar span').remove();
-// }
 
